116833a4db78d856fa912dcd844421b19c34a7d5
Eddie Bueno <eddiebueno93@gmail.com>
2019-04-29 08:42:08 -0700

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import MovieContext from './MovieContext'
+
+class HomePage extends React.Component {
+  
+  static contextType = MovieContext;
+
+  render(){  
+  return (
+    <h1>
+      <Link to='/' onClick={() => this.context.changeOrigin(true)}>Movie Manager</Link>
+    </h1>
+  );
+}
+}
+export default HomePage;

a12f24ae5d583ec07f7bf9fe0d34edc64762c7cd
Eddie Bueno <eddiebueno93@gmail.com>
2019-05-06 15:18:15 -0700

    imported provider into index

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from "react-router-dom";
+import {MovieProvider} from './context/MovieContext';
 
 ReactDOM.render(
   <BrowserRouter>
+    <MovieProvider>
     <App />
+    </MovieProvider>
   </BrowserRouter>
 , document.getElementById('root'));

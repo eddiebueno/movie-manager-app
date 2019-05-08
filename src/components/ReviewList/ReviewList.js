1b4f01533be514f512b1e53b5386ee88cd95df2e
Eddie Bueno <eddiebueno93@gmail.com>
2019-05-08 09:51:19 -0700

    added clear reviews to when review list unmounts

--- src/components/ReviewList/ReviewList.js
+++ src/components/ReviewList/ReviewList.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import MovieContext from '../../context/MovieContext';
+
+
 class ReviewList extends React.Component {
+  
+  static contextType = MovieContext;
+
+  componentWillUnmount(){
+    this.context.clearReviews();
+  }
   
   render(){
     let reviews =this.props.reviews;

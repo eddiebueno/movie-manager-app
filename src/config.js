e1c71599980d00ea89c965806b51456d52c97571
Eddie Bueno <eddiebueno93@gmail.com>
2019-05-09 14:52:32 -0700

    temp change to local host

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'postgres://fpnszckoxsvvev:350a3dc097de4f4249fe692b3877d13effa7012e4d5c2f48d828321060a88f12@ec2-54-235-208-103.compute-1.amazonaws.com:5432/d76gjhpheukp46',
-  // LOCAL_API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'postgres://fpnszckoxsvvev:350a3dc097de4f4249fe692b3877d13effa7012e4d5c2f48d828321060a88f12@ec2-54-235-208-103.compute-1.amazonaws.com:5432/d76gjhpheukp46',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'movie-app-client-auth-token',
 }
